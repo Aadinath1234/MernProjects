bf9b4c0ad2ed97f2cbadbcf03bb866173a36523d
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2024-12-07 21:31:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-     origin:'https://mernprojects-client.onrender.com',
+     origin:'http://localhost:5173/',
      credentials: true
 }
 
